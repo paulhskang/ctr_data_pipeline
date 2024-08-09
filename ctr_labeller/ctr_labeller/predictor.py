f9515a3010a1ee64aa7048e6d370f68379c51b2b
paulhskang <paulhskang@gmail.com>
2024-08-08 21:04:38 -0400

    implemented gui to verify images, finalized automatic post-processing algo

--- ctr_labeller/ctr_labeller/predictor.py
+++ ctr_labeller/ctr_labeller/predictor.py
@@ -101,6 +101,7 @@ class SAMBatchedPredictor:
             collected_batch_num = batch_data["collected_batch_num"][i].item()
             if self.data_saver.check_is_mask_processed(frame_id):
                 continue
+            # only update input prompts if offline masking
             self.update_input_prompts(frame_id, left_input_prompts, right_input_prompts)
             left_image_data = ImageData(batch_data["left_image"][i].cpu().detach().numpy(),
                                         batch_data["left_image_name"][i],
@@ -120,34 +121,20 @@ class SAMBatchedPredictor:
 
         run_frame_ids = [0, 29502, 45375, 49151, 69841, 69858, 85101, 100002]
         num_runs = len(run_frame_ids)
-        # run_left_input_prompts = [[829.98, 128.28],
-        #                            [984.19, 224.08],
-        #                             [1005.2, 240.44],
-        #                                [710.82, 175.01],
-        #                                  [750.54, 231.09],
-        #                                    [710.82, 172.68],
-        #                                      [727.17, 67.53]]
-        # run_right_input_prompts = [[740.02, 111.92],
-        #                             [681.61, 88.559],
-        #                               [662.92, 86.223],
-        #                                 [870.87, 109.59],
-        #                                   [728.34, 62.857],
-        #                                     [868.53, 111.92],
-        #                                       [749.37, 233.43]]    
         run_right_input_prompts = [[830, 128],
-                                   [984, 224],
-                                    [1005, 240],
-                                       [711, 175],
-                                         [751, 231],
-                                           [711, 173],
-                                             [727, 68]]
+            [984, 224],
+                [1005, 240],
+                    [875, 100],     #[871, 110],
+                        [728, 63],
+                            [869, 112],
+                                [727, 68]]
         run_left_input_prompts = [[740, 112],
-                                    [682, 89],
-                                      [663, 86],
-                                        [871, 110],
-                                          [728, 63],
-                                            [869, 112],
-                                              [749, 233]]    
+            [682, 89],
+                [663, 86],
+                    [705, 162],     #[711, 175],
+                        [751, 231],
+                            [711, 173],
+                                [749, 233]]
         
         for i in range(num_runs-1):
             if (frame_id >= run_frame_ids[i]) and (frame_id < run_frame_ids[i+1]):
