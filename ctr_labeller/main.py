b2dcd24beb6c3bb8e859651f9911c4cc42a44a8d
paulhskang <paulhskang@gmail.com>
2025-06-10 10:40:46 -0400

    moved arg_parser to main

--- ctr_labeller/main.py
+++ ctr_labeller/main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import torch
 import threading
+import argparse
 
-from ctr_labeller.arg_parser import arg_parser
 from ctr_labeller.dataset import StereoDataSet
 from ctr_labeller.datasaver import DataSaver
 from ctr_labeller.predictor import SAMBatchedPredictor
@@ -42,6 +42,23 @@ class SAMBatchedPredictorThread(threading.Thread):
             print("SAMBatchedPredictorThread | finished frame_ids: {} ".format(batch["frame_id"].cpu().detach().numpy()))
 
         print ("SAMBatchedPredictorThread | ------ BATCH IS DONE ------")
+
+def arg_parser():
+    ''' load CLI arguments '''
+    parser = argparse.ArgumentParser()
+    # positional arguments
+    parser.add_argument("--data-path",                      help="Directory containing reference file", type=str)
+    # optional arguments
+    parser.add_argument("--use-gui",                        help="User actively specifies which masks to save with the GUI", type=bool, default=True)
+    parser.add_argument("--input-prompt-json-name",         help="Name of input prompt file", type=str, default="")
+    parser.add_argument("--input-prompt-app-image-height",  help="Input prompt app image height", type=int, default=1080)
+    parser.add_argument("--batch-num",                      help="Process this batch number only", type=int, default=-1)
+    parser.add_argument("--save-image-appended-with-masks", help="Save images with mask overlays", type=bool, default=False)
+    parser.add_argument("--sort-based-on",                  help="Criteria on how masks are selected", type=str, default="None")
+    parser.add_argument("--max-size-to-add",                help="Number of images to load at one time for processing", type=int, default=40)
+
+    args = parser.parse_args()
+    return args
 
 def main():
     # Parse arguments
